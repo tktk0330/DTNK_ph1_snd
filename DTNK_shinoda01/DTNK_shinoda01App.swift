c8b18c0e2095a68ba256240c9cecbe0368cd3393
tktk0330 <tkmsnd0330@gmail.com>
2023-06-25 15:22:41 +0900

    Initial Commit

---
+++ DTNK_shinoda01/DTNK_shinoda01App.swift
@@ -0,0 +1,17 @@
+//
+//  DTNK_shinoda01App.swift
+//  DTNK_shinoda01
+//
+//  Created by Takuma Shinoda on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DTNK_shinoda01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
